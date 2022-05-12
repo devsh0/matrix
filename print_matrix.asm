90f3cbfa8744f072140c6d4fb0b117e2c26d3728
devsh0 <devashishjaiswal86@gmail.com>
2022-05-12 17:22:46 +0530

    Print the matrix

--- print_matrix.asm
+++ print_matrix.asm
@@ -1,5 +1,5 @@
 section .data
-element_fmt: db "%f ", 0
+element_fmt: db "%.3f", 9, 0
 
 section .text
 ; (row_base_ptr, row_size) ret void
@@ -11,15 +11,17 @@ fn_print_row:
     push r14
     sub rsp, 8
 
-    mov r12, rdi        ; row_base_ptr into r12
+    mov r12, rdi        ; row_base_ptr into r12 = 0x405ac0
     mov r13, rsi        ; row_size into r13
     mov r14, 0          ; element_index counter
 
 .loop:
     mov rdi, element_fmt
-    lea rsi, [r12 + r14 * 8]
-    xor eax, eax
+    lea rsi, [r12 + (r14 * 8)]
+    movsd xmm0, [rsi]
+    mov eax, 1
     call printf wrt ..plt
+    add r14, 1
     cmp r13, r14
     jg .loop
 
@@ -33,7 +35,15 @@ fn_print_row:
     ret
 
 section .data
-line_feed: db "%s", 0
+line_feed: db 10, 0
+
+fn_print_newline:
+    sub rsp, 8
+    mov rdi, line_feed
+    xor eax, eax
+    call printf wrt ..plt
+    add rsp, 8
+    ret
 
 section .text
 ; (matrix_base_ptr, matrix_size) ret void
@@ -43,32 +53,30 @@ fn_print_matrix:
     push r12
     push r13
     push r14
-    sub rsp, 8
+    push r15
 
     mov r12, rdi                ; matrix_base_ptr into r12
     mov r13, rsi                ; row_size into r13
+    lea r15, [r13 * 8]          ; row_size_in_bytes
     mov r14, 0                  ; row_index into r14
     mov rdi, r13
 
+    call fn_print_newline
+
 .loop:
-    imul rdi, 8                 ; row_size_in_bytes
+    mov rdi, r15
     imul rdi, r14
-    add rdi, r12                ; row_base_ptr
+    add rdi, r12
     mov rsi, r13
     call fn_print_row
     add r14, 1
+    call fn_print_newline
     cmp r13, r14
-
-.print_line_feed:
-    mov rdi, line_feed
-    mov rsi, 10
-    xor eax, eax
-    call printf wrt ..plt
     jg .loop
 
 .exit:
     xor eax, eax
-    add rsp, 8
+    pop r15
     pop r14
     pop r13
     pop r12
